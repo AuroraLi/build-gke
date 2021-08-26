91c55879ef558e130aaaf53a8dd35126645ba2e0
aurora <aurora.li61@gmail.com>
2021-08-26 20:15:59 +0000

    add Windows node pool

---
+++ terraform/windows_pool.tf
@@ -0,0 +1,14 @@
+resource "google_container_node_pool" "windows_pool" {
+  name               = "windows-pool"
+  project            = google_container_cluster.dyson_cluster.project
+  cluster            = google_container_cluster.dyson_cluster.name
+  location           = google_container_cluster.dyson_cluster.location
+
+  node_config {
+    machine_type = "e2-standard-4"
+    image_type   = "WINDOWS_LTSC" # Or WINDOWS_SAC for new features.
+  }
+
+  depends_on = [google_container_cluster.dyson_cluster]
+}
+
